841f82529771abc09c5b4d5aca4ed179fbff6d66
Fanash Beauty <pathanhuda80@gmail.com>
2024-10-30 17:55:27 +0530

    Grid Layout added to all the tabs

--- app/src/main/java/com/example/training/FragmentBawaseer.kt
+++ app/src/main/java/com/example/training/FragmentBawaseer.kt
@@ -5,17 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FragmentBawaseer : Fragment() {
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dev, container, false)
+        return inflater.inflate(R.layout.fragment_bawaseer, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.gridlayout)
+
+        val grid= listOf(
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12"),
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12"),
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12"),
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12")
+        )
+        recyclerView.layoutManager= GridLayoutManager(view.context, 2)
+        recyclerView.adapter = ImageAdapter(grid) { label ->
+            // Handle button clicks here
+        }
     }
 
 }
--- app/src/main/java/com/example/training/FragmentBook.kt
+++ app/src/main/java/com/example/training/FragmentBook.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FragmentBook : Fragment() {
 
@@ -15,7 +16,27 @@ class FragmentBook : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dev, container, false)
+        return inflater.inflate(R.layout.fragment_book, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.gridlayout)
+
+        val grid= listOf(
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12"),
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12"),
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12"),
+            Items(R.drawable.item_1, "Clock", "$12"),
+            Items(R.drawable.item_2, "Clock", "$12")
+        )
+        recyclerView.layoutManager= GridLayoutManager(view.context, 2)
+        recyclerView.adapter = ImageAdapter(grid) { label ->
+            // Handle button clicks here
+        }
+    }
 }
